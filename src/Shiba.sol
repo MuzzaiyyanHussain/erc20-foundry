9a660d2fbde11d7288e94a3718f8394073221f59
Muzzaiyyan Hussain <muzzaiyyan.web3@gmail.com>
2025-05-03 15:16:21 +0000

    initial commit

---
+++ src/Shiba.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.29;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Shiba is ERC20, Ownable {
+    constructor() ERC20("SHIBA", "SHIB") Ownable(msg.sender) {}
+
+    function mint(address _to, uint _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
+}
